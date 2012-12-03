11572403c4b8eefff27aa5ee361586301feb66d9
Lepko <lepko.san@gmail.com>
2012-12-03 18:58:53 +0100

    Updated to Minecraft version 1.4.5

--- src/common/net/lepko/easycrafting/helpers/VersionHelper.java
+++ src/common/net/lepko/easycrafting/helpers/VersionHelper.java
@@ -14,7 +14,7 @@ public class VersionHelper {
 		LATEST, OUTDATED, FAILED, DISABLED
 	}
 
-	public static final String VERSION = "1.1.1";
+	public static final String VERSION = "1.1.2";
 	public static final String MOD_NAME = "Easy Crafting";
 	public static final String MOD_ID = "Lepko-EasyCrafting";
 
