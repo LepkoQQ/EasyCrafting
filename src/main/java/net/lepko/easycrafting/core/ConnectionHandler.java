a33eda6438fa365d7f2eebbb385629292a2756c9
LepkoQQ <lepko.san@gmail.com>
2014-05-19 19:08:18 +0200

    some more packet fixing

--- src/main/java/net/lepko/easycrafting/core/ConnectionHandler.java
+++ src/main/java/net/lepko/easycrafting/core/ConnectionHandler.java
@@ -1,29 +1,26 @@
 package net.lepko.easycrafting.core;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.gameevent.PlayerEvent;
+import cpw.mods.fml.common.network.FMLNetworkEvent;
+import cpw.mods.fml.common.network.internal.FMLProxyPacket;
+import net.lepko.easycrafting.config.ConfigHandler;
+import net.lepko.easycrafting.network.packet.PacketServerConfig;
+import net.lepko.easycrafting.proxy.Proxy;
 
 public class ConnectionHandler {
 
     @SubscribeEvent
-    public void playerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
-        EasyLog.warning("PlayerLoggedInEvent RECEIVED");
-        // on server
+    public void playerLoggedIn(FMLNetworkEvent.ServerConnectionFromClientEvent event) {
         //TODO: packets
-        //        PacketServerConfig packet = new PacketServerConfig();
-        //        Packet250CustomPayload p250 = PacketDispatcher.getPacket(VersionHelper.MOD_ID, packet.getBytes());
-        //        manager.addToSendQueue(p250);
+        PacketServerConfig packet = new PacketServerConfig();
+        FMLProxyPacket pkt = new FMLProxyPacket(packet.getBytes(), VersionHelper.MOD_ID);
+        event.manager.scheduleOutboundPacket(pkt.toS3FPacket());
     }
 
     @SubscribeEvent
-    public void connectionClosed(PlayerEvent.PlayerLoggedOutEvent event) {
-        EasyLog.warning("PlayerLoggedOutEvent RECEIVED");
-
-        // on client
-        //TODO: packets
-        //        if (FMLCommonHandler.instance().getEffectiveSide().isClient()) {
-        //            ConfigHandler.initialize(null);
-        //            Proxy.proxy.replaceRecipe(ConfigHandler.CUSTOM_RECIPE_INGREDIENTS);
-        //        }
+    public void clientDisconnect(FMLNetworkEvent.ClientDisconnectionFromServerEvent event) {
+        //TODO: this is called in "Netty Client IO #0" thread; issue?
+        ConfigHandler.initialize(null);
+        Proxy.proxy.replaceRecipe(ConfigHandler.CUSTOM_RECIPE_INGREDIENTS);
     }
 }
--- src/main/java/net/lepko/easycrafting/core/TickHandlerClient.java
+++ src/main/java/net/lepko/easycrafting/core/TickHandlerClient.java
@@ -3,6 +3,7 @@ package net.lepko.easycrafting.core;
 import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
+import cpw.mods.fml.relauncher.Side;
 import net.lepko.easycrafting.inventory.gui.GuiEasyCrafting;
 import net.lepko.easycrafting.recipe.RecipeWorker;
 import net.minecraft.client.Minecraft;
@@ -25,34 +26,36 @@ public class TickHandlerClient {
 
     @SubscribeEvent
     public void tickEnd(TickEvent event) {
-        if (updateEasyCraftingOutput && count <= 0 && event.type == TickEvent.Type.CLIENT && event.phase == TickEvent.Phase.END) {
+        if (event.side == Side.CLIENT && event.type == TickEvent.Type.CLIENT) {
+            if (updateEasyCraftingOutput && count <= 0 && event.phase == TickEvent.Phase.END) {
+                if (RecipeWorker.lock.tryLock()) {
+                    try {
+                        RecipeWorker.instance().requestNewRecipeList();
+                        updateEasyCraftingOutput = false;
+                    } finally {
+                        RecipeWorker.lock.unlock();
+                    }
+                }
+                if (showUpdateInChat) {
+                    VersionHelper.printToChat();
+                    showUpdateInChat = false;
+                }
+            } else if (count > 0) {
+                count--;
+            }
+
             if (RecipeWorker.lock.tryLock()) {
                 try {
-                    RecipeWorker.instance().requestNewRecipeList();
-                    updateEasyCraftingOutput = false;
+                    if (mc.theWorld != null && RecipeWorker.instance().refreshDisplay()) {
+                        if (mc.currentScreen != null && mc.currentScreen instanceof GuiEasyCrafting) {
+                            GuiEasyCrafting gec = (GuiEasyCrafting) mc.currentScreen;
+                            gec.refreshCraftingOutput();
+                            RecipeWorker.instance().setDisplayed();
+                        }
+                    }
                 } finally {
                     RecipeWorker.lock.unlock();
                 }
-            }
-            if (showUpdateInChat) {
-                VersionHelper.printToChat();
-                showUpdateInChat = false;
-            }
-        } else if (count > 0) {
-            count--;
-        }
-
-        if (RecipeWorker.lock.tryLock()) {
-            try {
-                if (mc.theWorld != null && RecipeWorker.instance().refreshDisplay()) {
-                    if (mc.currentScreen != null && mc.currentScreen instanceof GuiEasyCrafting) {
-                        GuiEasyCrafting gec = (GuiEasyCrafting) mc.currentScreen;
-                        gec.refreshCraftingOutput();
-                        RecipeWorker.instance().setDisplayed();
-                    }
-                }
-            } finally {
-                RecipeWorker.lock.unlock();
             }
         }
     }
