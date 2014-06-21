d6e414696267a26095374c2b86c216b3506aa04d
LepkoQQ <lepko.san@gmail.com>
2014-06-21 17:14:51 +0200

    travis (get the correct build number) and print to file current build number and commit

--- upload_files_travis.sh
+++ upload_files_travis.sh
@@ -4,6 +4,8 @@ dir="./build/libs"
 if [ -d "$dir" ]
 then
    cd "$dir"
+   curl --remote-name http://mods.lepko.net/archive/travis/easycrafting/buildinfo.txt
+   echo "$TRAVIS_BUILD_NUMBER,$TRAVIS_COMMIT" >> buildinfo.txt
    find . -type f -exec curl --user $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://lepko.net/domains/lepko.net/public_html/mods/archive/travis/easycrafting/{} \;
 else
    echo "Error: $dir not found."
