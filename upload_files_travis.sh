65931d042c03a04971f0e3373f1df67daf79b5dd
LepkoQQ <lepko.san@gmail.com>
2014-06-21 16:44:01 +0200

    travis (how about actually changing directory -.-)

--- upload_files_travis.sh
+++ upload_files_travis.sh
@@ -3,6 +3,7 @@ dir="./build/libs"
 
 if [ -d "$dir" ]
 then
+   cd "$dir"
    find . -type f -exec curl --user $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://lepko.net/domains/lepko.net/public_html/mods/archive/travis/easycrafting/{} \;
 else
    echo "Error: $dir not found."
