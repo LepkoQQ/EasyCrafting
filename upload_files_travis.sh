c3e1ab1fe6e22d69e4083e75e45e6f9aace7f32e
LepkoQQ <lepko.san@gmail.com>
2014-06-22 00:51:01 +0200

    travis: create file with commit hash gradle: build dev and source jars

--- upload_files_travis.sh
+++ upload_files_travis.sh
@@ -4,8 +4,9 @@ dir="./build/libs"
 if [ -d "$dir" ]
 then
    cd "$dir"
-   curl --remote-name http://mods.lepko.net/archive/travis/easycrafting/buildinfo.txt
-   echo "$TRAVIS_BUILD_NUMBER,$TRAVIS_COMMIT" >> buildinfo.txt
+   THEFILE=$(ls -t *.jar | head -n 1)
+   THEFILE=${THEFILE/.jar/.commit}
+   echo $TRAVIS_COMMIT > $THEFILE
    find . -type f -exec curl --user $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://lepko.net/domains/lepko.net/public_html/mods/archive/travis/easycrafting/{} \;
 else
    echo "Error: $dir not found."
